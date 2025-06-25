440ec7e6321ff893b97f25972c6a6ca22b673436
pradyumna0104 <sahumanika874@gmail.com>
2025-06-25 05:59:35 +0530

    25 JUN 05:59AM

---
+++ video 54/script.js
@@ -0,0 +1,71 @@
+alert("Hello World.");
+
+console.log("Code is running...");
+console.log("Code is also running...");
+console.log("Code is looking like a wow...");
+
+
+
+
+var a  = prompt ("Enter a number: ");
+
+console.log("Your number is " + a);
+
+
+
+
+var isTrue = confirm("Are you sure want to leave this page and blast your computer.");
+
+if(isTrue){
+    console.log("Your computer is blasting..");
+}else{
+    console.log("Your computer is  not blasting..");
+}
+
+
+document.title = "Hey I am Good."
+document.body.style.backgroundColor = "lightgreen"
+
+
+// PS D:**********\Sigma-web-Dev-Course\video 54> node new.js  
+// Code is running...
+// Code is also running...
+// Code is looking like a wow...
+// PS D:**********\Sigma-web-Dev-Course\video 54> npm
+// npm <command>
+
+// Usage:
+
+// npm install        install all the dependencies in your project
+// npm install <foo>  add the <foo> dependency to your project
+// npm test           run this project's tests
+// npm run <foo>      run the script named <foo>
+// npm <command> -h   quick help on <command>
+// npm -l             display usage info for all commands
+// npm help <term>    search for help on <term> (in a browser)
+// npm help npm       more involved overview (in a browser)
+
+// All commands:
+
+//     access, adduser, audit, bugs, cache, ci, completion,
+//     config, dedupe, deprecate, diff, dist-tag, docs, doctor,
+//     edit, exec, explain, explore, find-dupes, fund, get, help,
+//     help-search, hook, init, install, install-ci-test,
+//     install-test, link, ll, login, logout, ls, org, outdated,
+//     owner, pack, ping, pkg, prefix, profile, prune, publish,
+//     query, rebuild, repo, restart, root, run-script, sbom,
+//     search, set, shrinkwrap, star, stars, start, stop, team,
+//     test, token, uninstall, unpublish, unstar, update, version,
+//     view, whoami
+
+// Specify configs in the ini-formatted file:
+//     C:******************
+// or on the command line via: npm <command> --key=value
+
+// More configuration info: npm help config
+// Configuration fields: npm help 7 config
+
+// npm@10.9.2 C:\Program Files\nodejs\node_modules\npm
+// PS D:***********\Sigma-web-Dev-Course\video 54> npm --version
+// 10.9.2
+// PS D:***********\Sigma-web-Dev-Course\video 54> 
